e0513b28d7c38d571d1ec88741f98343b536c788
Mateus Campos <mateus.camphos2000@gmail.com>
2025-05-25 18:20:43 -0300

    Create Admin.

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from school.models import Student, Course
 
-# Register your models here.
+class Students(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'cpf', 'date_of_birth', 'phone_number')
+    list_display_links = ('id', 'name',)
+    list_per_page = 20
+    search_fields = ('name',)
+
+admin.site.register(Student, Students)
+
+class Courses(admin.ModelAdmin):
+    list_display = ('id', 'code', 'description')
+    list_display_links = ('id', 'code',)
+    search_fields = ('code',)
+
+admin.site.register(Course, Courses)

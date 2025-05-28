8d1a5f8c30de939a5382a993db0584e039d4a769
Mateus Campos <mateus.camphos2000@gmail.com>
2025-05-28 11:14:55 -0300

    Configuring urls.

--- setup/urls.py
+++ setup/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from django.urls import path
-from school.views import students
+from django.urls import path, include
+from school.views import StudentViewSet, CourseViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('students', StudentViewSet, basename='Students')
+router.register('courses', CourseViewSet, basename='Courses')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('students/', students),
+    path('', include(router.urls)),
 ]

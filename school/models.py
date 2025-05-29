0b18d35210cf7da4656a731f7a9c83902e5b2da5
Mateus Campos <mateus.camphos2000@gmail.com>
2025-05-29 11:25:21 -0300

    Add registration model.

--- school/models.py
+++ school/models.py
@@ -23,3 +23,14 @@ class Course(models.Model):
 
     def __str__(self):
         return self.code
+
+class Registration(models.Model):
+    PERIOD = (
+        ('M', 'Matutino'),
+        ('V', 'Vespertino'),
+        ('N', 'Noturno'),
+    )
+    student = models.ForeignKey(Student, on_delete = models.CASCADE)
+    course = models.ForeignKey(Course, on_delete = models.CASCADE)
+    period = models.CharField(max_length = 1, choices = PERIOD, blank = False, null = False, default = 'M')
+    
